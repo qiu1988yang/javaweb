de2e6fddffa7e9295b92651c1327e36a30bd4ed7
qiuyang <qiuyang@qq.com>
2021-07-15 11:41:44 +0800

    no message

--- src/main/java/com/qy/demo/demo/service/DepartService.java
+++ src/main/java/com/qy/demo/demo/service/DepartService.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public interface DepartService {
 
-
     public List<Depart> selectAll();
 
 }
--- src/main/java/com/qy/demo/demo/service/Impl/DepartServiceImpl.java
+++ src/main/java/com/qy/demo/demo/service/Impl/DepartServiceImpl.java
@@ -3,16 +3,18 @@ package com.qy.demo.demo.service.Impl;
 import com.qy.demo.demo.bean.Depart;
 import com.qy.demo.demo.mapper.DepartMapper;
 import com.qy.demo.demo.service.DepartService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Service("departService")
-
-
 public class DepartServiceImpl implements DepartService {
 
+    @Resource
     private DepartMapper departMapper;
+
 
     @Override
     public List<Depart> selectAll() {
